d1e6b2e4178a49be2c19be41e2e4b218fc63ce41
EnjoyCpp <63050200+EnjoyCpp@users.noreply.github.com>
2020-04-28 01:02:17 +0300

    Clearing errors

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 
 int main(){
     Server s("https://solsa.crystallography.net/db/test.db");
-  src-edits
     /*Resource r = s.get_one("bibliography","2");
     std::cout << r.get_attribute("year").asString() << std::endl;
     std::cout << r.get_data().toStyledString()<<std::endl;*/
@@ -15,10 +14,6 @@ int main(){
     Resource r=s.get_all("bibliography");
     std::cout << r.get_data_one().toStyledString()<<std::endl;
 
-
-    Resource r = s.get_one("bibliography","2");
-    std::cout << r.get_attribute("title").asString() << std::endl;
- master
 
     
    return 0;
--- src/server.h
+++ src/server.h
@@ -16,10 +16,7 @@ class Server{
 	std::string set_url(const std::string url_new); //setting url for a server
 	std::string get_url();
 
- src-edits
 	Resource get_all(const std::string type); 
-	std::string get_all(const std::string type); 
- master
 	Resource get_one(const std::string type,const std::string id);
 
 	~Server();
