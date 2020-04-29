d46e1b7fbef27beda484bc83460af3085234d5be
EnjoyCpp <63050200+EnjoyCpp@users.noreply.github.com>
2020-04-29 14:56:52 +0300

    Adding additional setters and getters

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,15 @@ int main(){
     Resource r = s.get_one("bibliography","2");
     std::cout << r.get_attribute("year").asString() << std::endl;
     r.set_attribute("year",2020); //changing attribute value of attribute "year" to check is it working
+    r.set_id("5");
+    r.set_type("abc");
     std::cout << r.get_attribute("year").asString() << std::endl;
+    std::cout << r.get_id().asString() << std::endl;
+    std::cout << r.get_type().asString() << std::endl;
+
+
+
+
     std::cout << r.get_data().toStyledString()<<std::endl;
 
     /*Resource r=s.get_all("bibliography");
