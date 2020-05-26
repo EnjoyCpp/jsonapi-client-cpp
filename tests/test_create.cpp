db6aa687b20da1f7035340b1486fff450376039f
EnjoyCpp <63050200+EnjoyCpp@users.noreply.github.com>
2020-05-26 14:52:35 +0300

    Add files via upload

--- tests/test_create.cpp
+++ tests/test_create.cpp
@@ -10,10 +10,9 @@ int main(){
   Server s("http://jsonapiplayground.reyesoft.com/v2");
   Resource r = s.get_one("authors","2");
   r.unset_id();
-  //std::cout<<r.store(); //output of http://jsonapiplayground.reyesoft.com/v2/authors/2" is http response code: 405
   if(r.create()){
-	return 0; }
+	return 1; }
   
-    return 1;
+    return 0;
 };
 
