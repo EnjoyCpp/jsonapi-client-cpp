8d64aa8ae087c86cc062f44573ad3e3db9a62322
EnjoyCpp <63050200+EnjoyCpp@users.noreply.github.com>
2020-07-03 23:17:46 +0300

    Renaming method

--- tests/test_delete.cpp
+++ tests/test_delete.cpp
@@ -9,7 +9,7 @@
 int main(){
   Server s("http://jsonapiplayground.reyesoft.com/v2");
   Resource r = s.get_one("authors","2");
-  if(r.deleteResource("authors","30")){
+  if(r.del("authors","30")){
 	return 0; }
   
     return 1;
